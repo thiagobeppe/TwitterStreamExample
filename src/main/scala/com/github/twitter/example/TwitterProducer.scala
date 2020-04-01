be0561e677093804d39c618d279866071a0b241e
thiagobeppe <thiagoabeppe@gmail.com>
2020-04-01 17:54:57 -0300

    WIP kafka twiiter producer

--- src/main/scala/com/github/twitter/example/TwitterProducer.scala
+++ src/main/scala/com/github/twitter/example/TwitterProducer.scala
@@ -1,19 +1,25 @@
 package com.github.twitter.example
 
 import com.github.twitter.example.utils.Utils
-import twitter4j.{FilterQuery, TwitterFactory, TwitterStreamFactory}
+import twitter4j.{FilterQuery, TwitterStreamFactory}
 
 
 
 object TwitterProducer extends App {
 
-  val twitterStream = new TwitterStreamFactory(Utils.config).getInstance
+  var finishingLoop = true
 
+  //Create a twitter connect
+  val twitterStream = new TwitterStreamFactory(Utils.config).getInstance
   twitterStream.addListener(Utils.simpleStatusListener)
-  twitterStream.filter(new FilterQuery(Array(1344951,5988062,807095,3108351)))
-  Thread.sleep(10000)
+  //Create a kafka topic
+
+  //Create a loop to send msgs to kafka
+  while(finishingLoop){
+
+    Thread.sleep(10000)
+  }
   twitterStream.cleanUp
   twitterStream.shutdown
-
 
 }
